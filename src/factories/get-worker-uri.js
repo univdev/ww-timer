0f9d573cad7676889814d260fa22476c7b8e0b41
Chanyoung Park <pcydeveloper@gmail.com>
2024-03-27 19:39:27 +0900

    Feat: created WW Timer

---
+++ src/factories/get-worker-uri.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @param {string} workerScript 
+ */
+export const getWorkerURI = (workerScript) => {
+  if (typeof workerScript !== 'string') {
+    throw new Error('Worker script must be a string');
+  }
+
+  const blob = new Blob([workerScript], { type: 'text/javascript' });
+  const url = URL.createObjectURL(blob);
+
+  window.setTimeout(() => URL.revokeObjectURL(url));
+
+  return url;
+}
---
+++ src/worker.orginal.js
@@ -0,0 +1,15 @@
+(() => {
+  self.addEventListener('message', (event) => {
+    if (typeof event.data !== 'object') {
+      throw new Error('Invalid data');
+    }
+    
+    if (typeof event.data.interval !== 'number') {
+      throw new Error('Invalid interval');
+    }
+  
+    self.setInterval(() => {
+      self.postMessage('called');
+    }, event.data.interval);
+  });
+})();
